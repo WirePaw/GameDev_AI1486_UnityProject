fd156258daa6d37b3f8aa7a8bba8f1320be15bf7
johannab02 <johanna.broecher@unitybox.de>
2024-01-22 12:25:19 +0100

    added sounds

--- Assets/scripts/Can_be_collected.cs
+++ Assets/scripts/Can_be_collected.cs
@@ -7,13 +7,21 @@ using UnityEngine;
 
 public class Can_be_collected : MonoBehaviour
 {
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player"))
         {
             if(gameObject.tag.Equals("Key"))
             {
                 LevelManager.decreaseNeededKeys();
+                source.Play();
             }
             Destroy(this.gameObject);
         }
