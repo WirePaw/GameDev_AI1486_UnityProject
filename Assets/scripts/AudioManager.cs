4b4fdd5a9333ce615b3615fbdde3428e4a1d1fde
MaryRead1412 <jungautor@web.de>
2024-01-22 18:45:45 +0100

    added AudioManager to Scene
    created an AudioManager-system, to play sounds of the game. When collecting a key, a sound is now playing

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSound[] sounds;
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (AudioSound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+        }
+
+    }
+    public void Play(string name)
+    {
+        AudioSound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
