23e4c8944504def539482477668b001183d72bfb
MaryRead1412 <jungautor@web.de>
2024-02-04 11:32:35 +0100

    trying to create a main menu

---
+++ Assets/scripts/ButtonManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void doSomethingButton()
+    {
+        Debug.Log("I was pressed!!! HELL YEAH!");
+    }
+}
